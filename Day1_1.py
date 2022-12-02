51ad00e6e36f354a9235574c9ff333fe90786a2b
Milliegw <32912940+Milliegw@users.noreply.github.com>
2022-12-02 09:52:42 +0000

    Update Day1_1.py

--- Day1_1.py
+++ Day1_1.py
@@ -1,1 +1,26 @@
 
+count = 1
+values = []
+newlist = []
+amount = 0
+total_calories = 0
+
+with open ('input.txt', 'r') as file:
+    lines = file.read().split('\n\n')
+    file = [line.split() for line in lines]
+    x = [[int(j) for j in i] for i in file]
+    # print(x)
+
+# print(type(x))
+
+for l in x:
+    total_calories = sum(l)
+    print(f'{l} = {total_calories}')
+
+for l in x:
+    total_calories = sum(l)
+    if total_calories > amount:
+        amount = total_calories
+        total_calories = 0
+
+print(amount)
